77116d0f988295d3e83183abfa27d85d0261b3a7
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-03-31 14:56:57 +0300

    Move assistants to package

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from OpenAIAssistant import OpenAIAssistant
+from assistants.open_ai import OpenAIAssistant
 
-if __name__ == '__main__':
-    ass = OpenAIAssistant(model='gpt-4-0125-preview', name='Erkki',
-                          instructions="You answer the user's questions in haiku form")
-    response = ass.get_response(asker_name='konso', query="Who's the president of France?")
+if __name__ == "__main__":
+    ass = OpenAIAssistant(
+        model="gpt-4-0125-preview",
+        name="Erkki",
+        instructions="You answer the user's questions in haiku form",
+    )
+    response = ass.get_response(
+        asker_name="konso", query="Who's the president of France?"
+    )
     print(response)
-
