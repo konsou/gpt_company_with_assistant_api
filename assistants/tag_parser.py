1f531d345a18d6a5bbae363bc45d76f395688ac4
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-01 22:37:40 +0300

    Add MessageTagParser

--- assistants/tag_parser.py
+++ assistants/tag_parser.py
@@ -1,4 +1,5 @@
 from html.parser import HTMLParser
+from typing import NamedTuple
 
 
 class ExecuteTagParser(HTMLParser):
@@ -18,3 +19,36 @@ class ExecuteTagParser(HTMLParser):
     def handle_data(self, data):
         if self.inside_execute:
             self.texts.append(data.strip())
+
+
+from html.parser import HTMLParser
+
+
+class TagMessage(NamedTuple):
+    recipient: str
+    content: str
+
+
+class MessageTagParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.inside_message = False
+        self.recipient = ""
+        self.messages: list[TagMessage] = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "message":
+            self.inside_message = True
+            for attr, value in attrs:
+                if attr == "recipient":
+                    self.recipient = value
+
+    def handle_endtag(self, tag):
+        if tag == "message":
+            self.inside_message = False
+
+    def handle_data(self, data):
+        if self.inside_message:
+            self.messages.append(
+                TagMessage(recipient=self.recipient, content=data.strip())
+            )
