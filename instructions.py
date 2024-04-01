70bf86ffcc6753cac9eeb504f245b1f612802a15
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-01 20:36:07 +0300

    Move instructions to separate file

---
+++ instructions.py
@@ -0,0 +1,5 @@
+DEVELOPER_INSTRUCTIONS = """You write code that fulfills the customer's requests. 
+
+I've set up an isolated linux dev env for you. 
+You can execute commands in your dev environment by enclosing them in <execute></execute> tags.
+"""
