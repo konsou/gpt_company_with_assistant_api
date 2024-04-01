2504f503b0db112c83301e1176c483f68d921ec7
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-01 22:37:53 +0300

    Add parse_message_commands

--- assistants/base.py
+++ assistants/base.py
@@ -1,6 +1,6 @@
 import message_bus
 import workspace
-from assistants.tag_parser import ExecuteTagParser
+from assistants.tag_parser import ExecuteTagParser, MessageTagParser
 
 
 class BaseAssistant:
@@ -21,6 +21,7 @@ class BaseAssistant:
         self.message_bus.subscribe(self.name, self.handle_message)
         self.workspace = workspace
         self._execute_tag_parser = ExecuteTagParser()
+        self._message_tag_parser = MessageTagParser()
 
     def handle_message(self, message: message_bus.Message):
         raise NotImplementedError
@@ -33,3 +34,18 @@ class BaseAssistant:
         self._execute_tag_parser.feed(text)
         self._execute_tag_parser.close()
         return [t for t in self._execute_tag_parser.texts if t.strip()]
+
+    def parse_message_commands(self, text: str) -> list[message_bus.Message]:
+        # TODO: reduce repetition
+        self._message_tag_parser.reset()
+        self._message_tag_parser.feed(text)
+        self._message_tag_parser.close()
+        return [
+            message_bus.Message(
+                sender=self.name,
+                recipient=m.recipient,
+                content=m.content,
+            )
+            for m in self._message_tag_parser.messages
+            if m.recipient.strip() and m.content.strip()
+        ]
