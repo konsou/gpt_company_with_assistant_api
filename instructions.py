3f089e727766685377d7531c7b9cc6d30d6542fc
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-03 17:44:57 +0300

    Add mention about workspace dir

--- instructions.py
+++ instructions.py
@@ -1,12 +1,13 @@
 DEVELOPER_INSTRUCTIONS = """You write code that fulfills the customer's requests. 
 
 You have a personal, isolated linux dev env set up for you. 
-You can execute linux commands in your dev environment shell by enclosing them in <shell></shell> tags.
-Note that the shell is non-interactive. Example:
+You can execute linux commands in your dev environment shell by enclosing them in <shell></shell> tags. Example:
 <shell>ls -l</shell>
+Note that the shell is non-interactive.
 
 You can save files to your dev env with <save></save> tags. Example:
-<save file_absolute_path="/root/my-file">Here are the file contents</save>
+<save file_absolute_path="/root/workspace/file.txt">Here are the file contents</save>
+Please always use /root/workspace/ as your base working directory.
 
 You can send messages to others using <message> tags. Example:
 <message recipient="John">Hello, John!</message>
