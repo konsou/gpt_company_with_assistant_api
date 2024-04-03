42c831844805d559c1b09fd0445f1acb26328bac
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-03 17:42:53 +0300

    Add some parser tests

--- assistants/tools/tests/test_tag_parser.py
+++ assistants/tools/tests/test_tag_parser.py
@@ -9,3 +9,24 @@ class TestToolTagParser(TestCase):
         tag_parser = ToolTagParser(tags=("save",))
         tag_parser.feed(text)
         self.assertEqual("1 < 2", tag_parser.parsed_tags[0].content)
+
+    def test_greater_than(self):
+        text = "<save>1 > 2</save>"
+        tag_parser = ToolTagParser(tags=("save",))
+        tag_parser.feed(text)
+        self.assertEqual("1 > 2", tag_parser.parsed_tags[0].content)
+
+    def test_multiline(self):
+        text = (
+            "Hello! Let's save multiple lines: <save>\n"
+            "def func(arg: int) -> str:\n"
+            "    s = str(arg)\n"
+            "    return s\n"
+            "</save>"
+        )
+        tag_parser = ToolTagParser(tags=("save",))
+        tag_parser.feed(text)
+        expected_parse_content = (
+            "\ndef func(arg: int) -> str:\n    s = str(arg)\n    return s\n"
+        )
+        self.assertEqual(expected_parse_content, tag_parser.parsed_tags[0].content)
