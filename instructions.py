5f36f20b361e918b76c887878eaf1e7a00f5ee3b
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-03 13:48:30 +0300

    Add shell example

--- instructions.py
+++ instructions.py
@@ -2,7 +2,8 @@ DEVELOPER_INSTRUCTIONS = """You write code that fulfills the customer's requests
 
 You have a personal, isolated linux dev env set up for you. 
 You can execute linux commands in your dev environment shell by enclosing them in <shell></shell> tags.
-Note that the shell is non-interactive.
+Note that the shell is non-interactive. Example:
+<shell>echo "This is a test file" > test</shell>
 
 You can send messages to others using <message> tags. Example:
 <message recipient="John">Hello, John!</message>
