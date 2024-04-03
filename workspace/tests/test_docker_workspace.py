e4a3baae2f391449347f1474bb576bd91d4cfc0c
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-03 13:30:57 +0300

    Clean tests

--- workspace/tests/test_docker_workspace.py
+++ workspace/tests/test_docker_workspace.py
@@ -8,25 +8,24 @@ import workspace.docker_workspace
 
 class TestDockerWorkspace(TestCase):
     @patch("workspace.docker_workspace.WORKSPACE_DIR", tempfile.mkdtemp())
+    def setUp(self):
+        self.workspace = workspace.docker_workspace.DockerWorkspace()
+        self.workspace_dir = workspace.docker_workspace.WORKSPACE_DIR
+
     def test_run_one_command(self):
-        ws = workspace.docker_workspace.DockerWorkspace()
-        workspace_dir = workspace.docker_workspace.WORKSPACE_DIR
-        print(workspace_dir)
-        result = ws.run_command("echo moi")
+        print(self.workspace_dir)
+        result = self.workspace.run_command("echo moi")
         self.assertEqual(result.content, "moi")
         self.assertEqual(result.status, 0)
-        ws.cleanup()
-        os.removedirs(workspace_dir)
 
-    @patch("workspace.docker_workspace.WORKSPACE_DIR", tempfile.mkdtemp())
     def test_maintain_state(self):
-        ws = workspace.docker_workspace.DockerWorkspace()
-        workspace_dir = workspace.docker_workspace.WORKSPACE_DIR
-        print(workspace_dir)
-        ws.run_command("mkdir /test-dir")
-        ws.run_command("cd /test-dir")
-        result = ws.run_command("pwd")
+        print(self.workspace_dir)
+        self.workspace.run_command("mkdir /test-dir")
+        self.workspace.run_command("cd /test-dir")
+        result = self.workspace.run_command("pwd")
         self.assertEqual(result.content, "/test-dir")
         self.assertEqual(result.status, 0)
-        ws.cleanup()
-        os.removedirs(workspace_dir)
+
+    def tearDown(self):
+        self.workspace.cleanup()
+        os.removedirs(self.workspace_dir)
