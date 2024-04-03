3d875f83503b7c0016786d44d403cd2d4bb96e45
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-03 15:06:58 +0300

    Update instructions with <save>

--- instructions.py
+++ instructions.py
@@ -3,7 +3,10 @@ DEVELOPER_INSTRUCTIONS = """You write code that fulfills the customer's requests
 You have a personal, isolated linux dev env set up for you. 
 You can execute linux commands in your dev environment shell by enclosing them in <shell></shell> tags.
 Note that the shell is non-interactive. Example:
-<shell>echo "This is a test file" > test</shell>
+<shell>ls -l</shell>
+
+You can save files to your dev env with <save></save> tags. Example:
+<save file_absolute_path="/root/my-file">Here are the file contents</save>
 
 You can send messages to others using <message> tags. Example:
 <message recipient="John">Hello, John!</message>
