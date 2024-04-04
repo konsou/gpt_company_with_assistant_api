20d652fbd2e5ded818fad8d0cb216d96d078e932
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-04-04 19:02:43 +0300

    Add generic color print function

--- text.py
+++ text.py
@@ -2,6 +2,12 @@ from typing import Callable
 
 from colorama import Fore, Style
 
+from colorama import Fore as Color
+
+
+def print_in_color(text: str, color: Color) -> None:
+    print(f"{color}{text}{Style.RESET_ALL}")
+
 
 def print_system_message(msg: str) -> None:
     print(f"{Fore.LIGHTCYAN_EX}{msg}{Style.RESET_ALL}")
@@ -18,7 +24,7 @@ def print_warning(msg: str) -> None:
 def print_in_box(text: str, print_function: Callable = print):
     length = len(text)
     top = f'┏{"━" * (length + 2)}┓'
-    middle = f'┃ {text} ┃'
+    middle = f"┃ {text} ┃"
     bottom = f'┗{"━" * (length + 2)}┛'
 
     print_function(top)
